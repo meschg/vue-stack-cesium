755e52b7f042e7ace9bd02b316042bfd7f757a4c
meschg <gerhard@mesch.me>
2021-04-25 09:59:43 +0200

    npm updates

--- src/elements/AppBar.vue
+++ src/elements/AppBar.vue
@@ -79,7 +79,7 @@ export default class AppBar extends AppProps {
   routerProp: string = "prop";
 
   dep = {
-    "@vue/composition-api": "1.0.0-rc.6",
+    "@vue/composition-api": "1.0.0-rc.7",
     axios: "0.21.1",
     cesium: "1.80.0",
     vue: "2.6.12",
@@ -87,7 +87,7 @@ export default class AppBar extends AppProps {
     "vue-property-decorator": "9.1.2",
     "vue-router": "3.5.1",
     "vue-shortkey": "3.1.7",
-    vuetify: "2.4.9",
+    vuetify: "2.4.11",
     vuex: "3.6.2",
     typescript: "4.2.4",
   };
