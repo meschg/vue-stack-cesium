0a61c37c9d4227251f5156d6455806637f3fb6e4
mesch <gerhard@mesch.me>
2021-07-03 10:48:46 +0200

    npm updates - cesium 1.83.0 & minors

--- src/elements/AppBar.vue
+++ src/elements/AppBar.vue
@@ -81,17 +81,17 @@ export default class AppBar extends AppProps {
   routerProp: string = "prop";
 
   dep = {
-    compositionAPI: "1.0.0-rc.12",
+    compositionAPI: "1.0.0-rc.13",
     axios: "0.21.1",
-    cesium: "1.82.1",
+    cesium: "1.83.0",
     vue: "2.6.14",
     "vue-class-component": "7.2.6",
     "vue-property-decorator": "9.1.2",
     "vue-router": "3.5.1",
     "vue-shortkey": "3.1.7",
-    vuetify: "2.5.5",
+    vuetify: "2.5.6",
     vuex: "3.6.2",
-    typescript: "4.3.4",
+    typescript: "4.3.5",
   };
 
   routerActions() {
