8065efce85aa56730cf328068efbae3d9f293397
Gerhard Mesch <gerhard@mesch.me>
2022-02-20 16:04:27 +0100

    Less is more - removed sass & sass-loader

--- src/App.vue
+++ src/App.vue
@@ -42,13 +42,14 @@ export default defineComponent({
 });
 </script>
 
+<style scoped lang="scss"></style>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000000;
 }
 
 nav {
